b8e3090e60dc44ba2e29837ba429933c2a763b8b
zerdhanRWS <cahdani399@gmail.com>
2021-12-28 16:51:30 +0700

    first commit

---
+++ Coba_Pyhton.py
@@ -0,0 +1,10 @@
+data = ['a','b','c','d','e','1','2','3','4','5']
+
+print("c" in(data))
+print("ab" in(data))
+print("12" in(data))
+
+data1 = ['1','2','3','4','5']
+
+print(type(data))
+print(data[3])
